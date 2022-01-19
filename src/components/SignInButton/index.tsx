fd95f5071a7ec8f9f8079c4201b2d93602256d04
Willian <will.haya@gmail.com>
2022-01-19 17:00:18 -0300

    start checkout process

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,14 +1,11 @@
 import { FaGithub } from "react-icons/fa"
-import { FiX} from 'react-icons/fi'
+import { FiX } from 'react-icons/fi'
 import { signIn, signOut, useSession } from "next-auth/client"
 
 import styles from "./styles.module.scss"
 
 export function SignInButton() {
   const [session] = useSession()
-
-  console.log(session)
-
   return session ? (
     <button
       type="button" 
--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,5 @@
+import { useSession, signIn } from 'next-auth/client';
+import { stripe } from '../../services/stripe';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps{
@@ -5,10 +7,22 @@ interface SubscribeButtonProps{
 }
 
 export function SubscribeButton({ priceId } : SubscribeButtonProps) {
+  const [session] = useSession();
+
+  function handleSubscribe(){
+    if(!session){
+      signIn('github')
+      return;
+    }
+
+  }
+
+
   return (
     <button
     type="button"
-    className={styles.subscribeButton}>
+    className={styles.subscribeButton}
+    onClick={handleSubscribe}>
       Subscribe now
     </button>
   )
