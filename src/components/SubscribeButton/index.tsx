4db2d48d0e036ca832ff0986a62474c58edb72e8
Willian <will.haya@gmail.com>
2022-03-07 17:05:12 -0300

    commit

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,7 @@
 import { useSession, signIn } from 'next-auth/client';
-import { stripe } from '../../services/stripe';
-import styles from './styles.module.scss'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js';
+import styles from './styles.module.scss';
 
 interface SubscribeButtonProps{
   priceId: string;
@@ -9,13 +10,25 @@ interface SubscribeButtonProps{
 export function SubscribeButton({ priceId } : SubscribeButtonProps) {
   const [session] = useSession();
 
-  function handleSubscribe(){
+  async function handleSubscribe(){
     if(!session){
       signIn('github')
       return;
     }
 
+  try{
+    const response = await api.post('/subscribe')
+
+    const { sessionId } = response.data;
+
+    const stripe = await getStripeJs()
+
+    await stripe.redirectToCheckout({ sessionId });
+
+  } catch(err){
+    alert(err.message);
   }
+}
 
 
   return (
